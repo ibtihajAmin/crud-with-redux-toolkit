63c645c579aa70a8804bf40a5be408e52a0442a9
ibtihajAmin <ibtihaj.amin18@gmail.com>
2023-07-04 13:14:43 +0600

    Update funcionality is added

--- src/component/Update.js
+++ src/component/Update.js
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { updateStudent } from "../reducer/StudentReducer";
+
 export default function Update() {
+  const { id } = useParams();
+  const students = useSelector((state) => state.students);
+  const existingStudent = students.filter((s) => s.id == id);
+  const { name, email, roll, fav_subject } = existingStudent[0];
+  const [updateName, setUpdateName] = useState(name);
+  const [updateEmail, setUpdateEmail] = useState(email);
+  const [updateRoll, setUpdateRoll] = useState(roll);
+  const [updateFavSubject, setUpdateFavSubject] = useState(fav_subject);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    dispatch(
+      updateStudent({
+        id: id,
+        name: updateName,
+        email: updateEmail,
+        roll: updateRoll,
+        fav_subject: updateFavSubject,
+      })
+    );
+    navigate("/");
+  };
+
   return (
     <div className="d-flex vh-100 justify-content-center align-items-center">
       <div className="w-50 border bg-dark text-white p-5">
         <h3>Update Student Information</h3>
-        <form>
+        <form onSubmit={handleUpdate}>
           <div>
             <label htmlFor="name">Name</label>
             <input
@@ -11,6 +42,8 @@ export default function Update() {
               name="name"
               className="form-control"
               placeholder="Enter Name"
+              value={updateName}
+              onChange={(e) => setUpdateName(e.target.value)}
             />
           </div>
           <div>
@@ -20,6 +53,8 @@ export default function Update() {
               name="email"
               className="form-control"
               placeholder="Enter Email"
+              value={updateEmail}
+              onChange={(e) => setUpdateEmail(e.target.value)}
             />
           </div>
           <div>
@@ -29,6 +64,8 @@ export default function Update() {
               name="roll"
               className="form-control"
               placeholder="Enter Roll Number"
+              value={updateRoll}
+              onChange={(e) => setUpdateRoll(e.target.value)}
             />
           </div>
           <div>
@@ -38,6 +75,8 @@ export default function Update() {
               name="favSubject"
               className="form-control"
               placeholder="Enter Your Favorite Subject"
+              value={updateFavSubject}
+              onChange={(e) => setUpdateFavSubject(e.target.value)}
             />
           </div>
           <br />
--- src/reducer/StudentReducer.js
+++ src/reducer/StudentReducer.js
@@ -2,14 +2,24 @@ import { createSlice } from "@reduxjs/toolkit";
 import { studentList } from "../data/data";
 
 const studentSlice = createSlice({
-  name: "student",
+  name: "students",
   initialState: studentList,
   reducers: {
     addStudent: (state, action) => {
       state.push(action.payload);
     },
+    updateStudent: (state, action) => {
+      const { id, name, email, roll, fav_subject } = action.payload;
+      const sf = state.find((student) => student.id == id);
+      if (sf) {
+        sf.name = name;
+        sf.email = email;
+        sf.roll = roll;
+        sf.fav_subject = fav_subject;
+      }
+    },
   },
 });
 
-export const { addStudent } = studentSlice.actions;
+export const { addStudent, updateStudent } = studentSlice.actions;
 export default studentSlice.reducer;
